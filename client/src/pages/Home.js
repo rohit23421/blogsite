1acc3a2e74dbf293137da25309552506d7c6935e
Rohit sah <61084486+rohit23421@users.noreply.github.com>
2021-11-20 21:40:27 +0530

    Add files via upload

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,7 +4,7 @@ const Home = () => {
   return (
     <div className="mb-20">
       <h1 className="sm:text-4xl text-2xl font-bold mt-6 mb-6 text-gray-900">
-        Hello, Welcome to my Blog
+        Hello, Welcome to my Blog!
       </h1>
       <p className="mx-auto leading-relaxed text-base mb-4">
         Welcome to my Blog! Lorem ipsum dolor sit amet consectetur adipisicing
